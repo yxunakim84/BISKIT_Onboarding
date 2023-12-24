a092f2648d293cc93e08b350d7739342bb39dc47
김유나 <yuna000804@gmail.com>
2023-12-24 19:26:29 +0900

    feat: add font, fontSize, colors, shadow

---
+++ src/app/theme/colors.js
@@ -0,0 +1,56 @@
+const bg = {
+  primaryWeak: "#FFF9DB",
+  primary: "#FFEC6E",
+  primaryStrong: "##FFE74D",
+  secondaryWeak: "#EFEFFE",
+  secondary: "#7880F0",
+  default: "#FFFFFF",
+  defaultActive: "rgba(33, 45, 105, 0.05)",
+  elevation1: "#F8FAFC",
+  elevation2: "#F1F4F9",
+  elevation3: "#E4E9F1",
+  inverseWeak: "#374154",
+  inverse: "#171C2B",
+  error: "#FDF0F0",
+  errorActive: "#FEDDDC",
+  overlay: "rgba(0, 0, 0, 0.7)",
+  dimmed: "rgba(0, 0, 0, 0.5)",
+  orange: "#FF6924",
+};
+
+const border = {
+  primary: "#FFE74D",
+  primaryStrong: "##FFD01C",
+  weak: "#F1F4F9",
+  default: "#E4E9F1",
+  strong: "#D3D9E2",
+  stronger: "##677389",
+  onBgInverse: "#374154",
+  error: "#DF2C22",
+};
+
+const content = {
+  default: "#171C2B",
+  onBgPrimary: "#171C2B",
+  inverse: "#FFFFFF",
+  weak: "#374154",
+  weaker: "#677389",
+  weakest: "#9CA6B8",
+  disabled: "#BCC4CF",
+  placeholder: "#BCC4CF",
+  inverseWeak: "#F8FAFC",
+  inverse: "#FFFFFF",
+  primary: "#F9B301",
+  primaryOnInverse: "#FFEC6E",
+  secondary: "#5B5DE3",
+  success: "#00993A",
+  error: "#DF2C22",
+};
+
+const colors = {
+  bg,
+  border,
+  content,
+};
+
+module.exports = colors;
