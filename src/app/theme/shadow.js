f192212f88eaa6140cdf03f2443eaf802900db32
김유나 <yuna000804@gmail.com>
2023-12-24 19:26:29 +0900

    feat: add font, fontSize, colors, shadow

---
+++ src/app/theme/shadow.js
@@ -0,0 +1,8 @@
+const boxShadow = {
+  "shadow-1": "0px 1px 4px 0px rgba(73, 91, 125, 0.07)",
+  "shadow-2":
+    "0px 4px 20px 0px rgba(73, 91, 125, 0.07), 0px 0px 1px 0px rgba(73, 91, 125, 0.03)",
+  "shadow-3": "0px 16px 40px 0px rgba(73, 91, 125, 0.18)",
+};
+
+module.exports = boxShadow;
