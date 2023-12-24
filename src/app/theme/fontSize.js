cfad44e07fa1d9eff5cf9b174ec313f792022f70
김유나 <yuna000804@gmail.com>
2023-12-24 19:26:29 +0900

    feat: add font, fontSize, colors, shadow

---
+++ src/app/theme/fontSize.js
@@ -0,0 +1,88 @@
+const fontSize = {
+  heading24Bd: [
+    "24px",
+    {
+      fontWeight: "700",
+      lineHeight: "140%",
+    },
+  ],
+  heading20Bd: [
+    "20px",
+    {
+      fontWeight: "700",
+      lineHeight: "140%",
+    },
+  ],
+  heading18Bd: [
+    "18px",
+    {
+      fontWeight: "700",
+      lineHeight: "140%",
+    },
+  ],
+  heading18Rg: [
+    "18px",
+    {
+      fontWeight: "400",
+      lineHeight: "140%",
+    },
+  ],
+  body16Sb: [
+    "16px",
+    {
+      fontWeight: "600",
+      lineHeight: "140%",
+    },
+  ],
+  body16Rg: [
+    "16px",
+    {
+      fontWeight: "400",
+      lineHeight: "140%",
+    },
+  ],
+  body14Sb: [
+    "14px",
+    {
+      fontWeight: "600",
+      lineHeight: "150%",
+    },
+  ],
+  body14Rg: [
+    "14px",
+    {
+      fontWeight: "400",
+      lineHeight: "150%",
+    },
+  ],
+  caption12Sb: [
+    "12px",
+    {
+      fontWeight: "600",
+      lineHeight: "150%",
+    },
+  ],
+  caption12Rg: [
+    "12px",
+    {
+      fontWeight: "400",
+      lineHeight: "150%",
+    },
+  ],
+  caption11Rg: [
+    "11px",
+    {
+      fontWeight: "400",
+      lineHeight: "150%",
+    },
+  ],
+  caption10Rg: [
+    "10px",
+    {
+      fontWeight: "400",
+      lineHeight: "150%",
+    },
+  ],
+};
+
+module.exports = fontSize;
