24f406e4d156e9f13a19388f3334cd8b19f174e7
김유나 <yuna000804@gmail.com>
2023-12-25 12:32:28 +0900

    feat: create third page

--- src/app/(viewport)/(public)/page.tsx
+++ src/app/(viewport)/(public)/page.tsx
@@ -2,14 +2,16 @@
 
 import Part1 from "@/components/Part1";
 import Part2 from "@/components/Part2";
+import Part3 from "@/components/Part3";
 import useScrollFadeIn, { DirectionType } from "@/hooks/useScrollFadeIn";
 import colors from "@/theme/colors";
 
 export default function Home() {
   return (
-    <main className="w-full flex flex-col min-h-screen items-center">
+    <main className="w-full flex flex-col items-center">
       <Part1 />
       <Part2 />
+      <Part3 />
     </main>
   );
 }
