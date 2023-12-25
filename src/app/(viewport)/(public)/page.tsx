33c2011641090579a169ea2e8aafa53b5399b2ef
김유나 <yuna000804@gmail.com>
2023-12-25 14:53:46 +0900

    feat: create fifth page

--- src/app/(viewport)/(public)/page.tsx
+++ src/app/(viewport)/(public)/page.tsx
@@ -4,6 +4,7 @@ import Part1 from "@/components/Part1";
 import Part2 from "@/components/Part2";
 import Part3 from "@/components/Part3";
 import Part4 from "@/components/Part4";
+import Part5 from "@/components/Part5";
 import useScrollFadeIn, { DirectionType } from "@/hooks/useScrollFadeIn";
 import colors from "@/theme/colors";
 
@@ -14,6 +15,7 @@ export default function Home() {
       <Part2 />
       <Part3 />
       <Part4 />
+      <Part5 />
     </main>
   );
 }
