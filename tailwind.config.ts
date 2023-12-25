b7714073733e514260f770b4f6002fded80a8a47
김유나 <yuna000804@gmail.com>
2023-12-25 16:45:16 +0900

    chore: customize screen size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,18 +12,16 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      mobile: "0px",
+      laptop: "700px",
+      desktop: "1441px",
+    },
     colors,
     fontSize,
     boxShadow,
     keyframes,
     animation,
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
   },
   plugins: [],
 };
