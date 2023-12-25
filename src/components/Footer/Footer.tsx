3c81e3076047e4f725fc4f6ebde9ea0006a52b56
김유나 <yuna000804@gmail.com>
2023-12-25 15:08:18 +0900

    refactor: separate `Header` and `Footer` component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Footer() {
+  return <div className="h-[120px] w-full bg-bg-elevation2" />;
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Icon from "../Icon";
+import colors from "@/theme/colors";
+
+export default function Header() {
+  return (
+    <section className="max-w-[1220px] w-full flex flex-col bg-bg-elevation2">
+      <header className="px-5 flex justify-between h-[72px] items-center">
+        <Icon
+          name="Logo"
+          width={51}
+          height={24}
+          color={colors.content.weaker}
+        />
+        <div className="flex h-fit items-center">
+          <button className="py-[10px] px-3 text-content-weak text-heading18Bd">
+            KOR
+          </button>
+          <span className="w-[1px] h-6 bg-border-default mx-2" />
+          <button className="py-[10px] px-3 text-content-weakest text-heading18Rg">
+            ENG
+          </button>
+        </div>
+      </header>
+    </section>
+  );
+}
