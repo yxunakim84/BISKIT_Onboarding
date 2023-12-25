1dbbd7aaf929d86a1591a785a82949f18635b25a
김유나 <yuna000804@gmail.com>
2023-12-25 11:26:36 +0900

    feat: create second page

--- src/app/(viewport)/(public)/page.tsx
+++ src/app/(viewport)/(public)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Part1 from "@/components/Part1";
+import Part2 from "@/components/Part2";
 import useScrollFadeIn, { DirectionType } from "@/hooks/useScrollFadeIn";
 import colors from "@/theme/colors";
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className="w-full flex flex-col min-h-screen items-center">
       <Part1 />
+      <Part2 />
     </main>
   );
 }
--- src/components/Icon/svgs/index.ts
+++ src/components/Icon/svgs/index.ts
@@ -1,3 +1,4 @@
 export { default as Logo } from "./biskit.svg";
 export { default as Apple } from "./apple_logo.svg";
 export { default as Google } from "./googleplay_logo.svg";
+export { default as Badge } from "./ic_verify_fill_24.svg";
