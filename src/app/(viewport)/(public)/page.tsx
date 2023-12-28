79af9593d6233ec25d7c81809f3d569c4ed04f98
김유나 <yuna000804@gmail.com>
2023-12-28 23:42:26 +0900

    feat: differentiate app download link based on the operating system

--- src/app/(viewport)/(public)/page.tsx
+++ src/app/(viewport)/(public)/page.tsx
@@ -9,18 +9,21 @@ import Part4 from "@/components/Part4";
 import Part5 from "@/components/Part5";
 import Part6 from "@/components/Part6";
 import Part7 from "@/components/Part7";
+import * as os from "os";
 
 export default function Home() {
+  const platform: NodeJS.Platform = os.platform();
+
   return (
     <main className="w-screen">
       <Header />
-      <Part1 />
+      <Part1 platform={platform} />
       <Part2 />
       <Part3 />
       <Part4 />
       <Part5 />
       <Part6 />
-      <Part7 />
+      <Part7 platform={platform} />
       <Footer />
     </main>
   );
