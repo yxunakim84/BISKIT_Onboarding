fb0e945324da42d9a6c7c11cc820b5712afd0805
김유나 <yuna000804@gmail.com>
2023-12-30 23:59:18 +0900

    style: create infinite horizontal scroll animation

--- src/theme/animation.js
+++ src/theme/animation.js
@@ -1,6 +1,7 @@
 const animation = {
   fadeIn: "fadeIn 2.5s ease-in-out",
   fadeOut: "fadeOut 2.5s ease-out",
+  infiniteScroll: "infiniteScroll 25s linear infinite",
 };
 
 module.exports = animation;
