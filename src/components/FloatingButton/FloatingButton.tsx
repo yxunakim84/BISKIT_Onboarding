f9b2f82f7403606aa8dfedcfbb140cb674c18ed6
김유나 <yuna000804@gmail.com>
2024-01-28 16:10:45 +0900

    chore: install and configure Prettier

--- src/components/FloatingButton/FloatingButton.tsx
+++ src/components/FloatingButton/FloatingButton.tsx
@@ -2,24 +2,21 @@
 
 import { PageData } from "@/contexts/locale.context/locale.context";
 import useLocale from "@/hooks/useLocale";
-import { Locale } from "@/types/locale";
-import React from "react";
 
 export default function FloatingButton() {
-  const { dict } = useLocale() as { dict: PageData };
+  const { dict } = useLocale() as { setLocale: any; dict: PageData };
 
   return (
     <div className="fixed bottom-0 w-full">
       <div
         className="h-6"
         style={{
-          background:
-            "linear-gradient(180deg, rgba(248, 250, 252, 0.00) 0%, #F8FAFC 100%",
+          background: "linear-gradient(180deg, rgba(248, 250, 252, 0.00) 0%, #F8FAFC 100%",
         }}
       />
-      <div className="bg-bg-elevation1 flex justify-center w-full h-[84px] pt-2 pb-5 mobile:px-5 laptop:px-10">
-        <button className="max-w-[1220px] bg-bg-primary p-4 text-heading18Bd w-full rounded-[12px]">
-          {dict.applyButton.title}
+      <div className="flex h-[84px] w-full justify-center bg-bg-elevation1 pb-5 pt-2 mobile:px-5 laptop:px-10">
+        <button className="w-full max-w-[1220px] rounded-[12px] bg-bg-primary p-4 text-heading18Bd">
+          {dict?.applyButton?.title}
         </button>
       </div>
     </div>
