9e012fa33659e1a2e7567456f27c7a8ddec28097
김유나 <yuna000804@gmail.com>
2024-01-28 16:10:45 +0900

    chore: install and configure Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tailwindFunctions: ['clsx', 'cva', 'cx'],
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
