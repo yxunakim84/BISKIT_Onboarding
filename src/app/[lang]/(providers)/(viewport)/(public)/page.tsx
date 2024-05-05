cea68eeb1a3a690a28e11a14a956db912581a7b5
김유나 <yuna000804@gmail.com>
2024-05-05 23:53:12 +0900

    fix: update GA4 code insertion, metadata

--- src/app/[lang]/(providers)/(viewport)/(public)/page.tsx
+++ src/app/[lang]/(providers)/(viewport)/(public)/page.tsx
@@ -1,28 +1,11 @@
 import PageDetail from "@/components/PageDetail";
 import { getDictionary } from "@/lib/dictionary";
-import Head from "next/head";
 
 export default async function Home({ params: { lang } }: { params: { lang: string } }) {
   const dict = await getDictionary((lang as "ko" | "en") || "en");
 
   return (
     <main className="w-screen">
-      <>
-        <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            
-            gtag('config', 'G-1B5D11GK85');
-            `,
-            }}
-          ></script>
-        </Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1B5D11GK85"></script>
-      </>
       <PageDetail dict={dict} lang={lang} />
     </main>
   );
--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,14 +2,21 @@ import { pretendard } from "@/theme/font";
 import { Locale } from "@/types/locale";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
+import Script from "next/script";
 import "../../app/globals.css";
 
 export const metadata: Metadata = {
-  title: "biskit",
+  title: "BISKIT | 글로벌한 대학생활의 시작",
+  description: "BISKIT에서 우리 학교의 외국인, 한국인 친구들과 교류해보세요!",
+  keywords:
+    "대학, 대학교, 대학생, 외국인, 외국인친구, 모임, 한국인친구, 언어교환, 학교, 언어, 취미, 관심사, 친구만들기, 점심, 밥, 소모임, 식사, 스터디, 20대, 교환학생, 친구, 한국인국제교류, 밋업, 글로벌, 캠퍼스",
+  icons: {
+    icon: "https://biskit-bucket.s3.ap-northeast-2.amazonaws.com/logo/biskit-image.png",
+  },
   openGraph: {
     images: [
       {
-        url: "https://file.notion.so/f/f/534465be-bf13-466e-858b-febadc997650/d3965ba6-6494-4e3f-97dd-5cea6dc70515/biskit_app_icon.png?id=b54fd1b1-ecdd-477c-8bf8-ba3f8dadd232&table=block&spaceId=534465be-bf13-466e-858b-febadc997650&expirationTimestamp=1703613600000&signature=3hwtAwVBfThNAD-0J-gxKOIcO67tttmBvwmmE9F6_9Q&downloadName=biskit_app_icon.png",
+        url: "https://biskit-bucket.s3.ap-northeast-2.amazonaws.com/logo/biskit-image.png",
       },
     ],
   },
@@ -18,6 +25,22 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, params }: { children: React.ReactNode; params: { lang: Locale } }) {
   return (
     <html lang={params.lang} className={pretendard.className}>
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1B5D11GK85"></Script>
+        <Script
+          async
+          strategy="afterInteractive"
+          id="analytics"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1B5D11GK85');
+          `,
+          }}
+        />
+      </head>
+
       <body id="body" className="h-screen antialiased">
         {children}
       </body>
