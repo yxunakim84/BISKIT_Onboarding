18cabd640519ae270f9f18916ed3820402dd10f4
김유나 <yuna000804@gmail.com>
2024-05-09 00:04:12 +0900

    fix: update fixes

--- src/app/[lang]/(providers)/(viewport)/layout.tsx
+++ src/app/[lang]/(providers)/(viewport)/layout.tsx
@@ -1,21 +1,16 @@
-import FloatingButton from "@/components/FloatingButton";
 import Header from "@/components/Header";
 
-export default function ViewportLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function ViewportLayout({ children }: { children: React.ReactNode }) {
   return (
     <div id="container-wrapper">
       <Header />
       <div
         id="container"
-        className="max-w-[1220px] items-center justify-center flex-col bg-bg-elevation2 relative mx-auto h-full flex min-h-screen w-full bg-white"
+        className="bg-white relative mx-auto flex h-full min-h-screen w-full max-w-[1220px] flex-col items-center justify-center bg-bg-elevation2"
       >
         {children}
       </div>
-      <FloatingButton />
+      {/* <FloatingButton /> */}
     </div>
   );
 }
